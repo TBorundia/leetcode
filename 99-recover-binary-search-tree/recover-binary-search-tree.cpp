e1957e2955725201c6f7abc5da43c5b06b1e66e4
Tina Borundia <168854094+TBorundia@users.noreply.github.com>
2025-08-04 12:48:03 +0530

    Time: 0 ms (100.00%) | Memory: 60.8 MB (58.24%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -11,40 +11,27 @@
  */
 class Solution {
 public:
-    void iot(TreeNode* root,vector<int> &ans){
-        if (root==NULL){return ;}
-        iot(root->left,ans);
-        ans.push_back(root->val);
-        iot(root->right,ans);
-    }
-    void fixem(TreeNode* root, unordered_map<int,TreeNode*>&mp){
-        if (root==NULL){return;}
-        mp[root->val]=root;
-        fixem(root->left,mp);
-        fixem(root->right,mp);
+    TreeNode* prev=NULL;
+    TreeNode* first=NULL;
+    TreeNode* middle=NULL;
+    TreeNode* last=NULL;
+    void inorder(TreeNode* root){
+        if(!root) return;
+        inorder(root->left);
+        if(prev!=NULL && root->val<prev->val){
+            if(first==NULL){
+                first=prev;
+                middle=root;
+            }
+            else last=root;
+        }
+        prev=root;
+
+        inorder(root->right);
     }
     void recoverTree(TreeNode* root) {
-        vector<int>ans;
-        iot(root,ans);
-        int a=0,b=0;
-        for (int i=0;i<ans.size();i++){cout<<ans[i]<<' ';}
-        cout<<endl;
-        vector<int>bns=ans;
-        sort(ans.begin(),ans.end());
-        for (int i=0;i<ans.size();i++){cout<<ans[i]<<' ';}
-        if (bns==ans){return;}
-        else {
-            for (int i=0;i<ans.size();i++){
-                if (ans[i]!=bns[i]){
-                    a=bns[i];
-                    b=ans[i];
-                    break;
-                }
-            }
-        }
-        unordered_map<int,TreeNode*> mp;
-        fixem(root,mp);
-        mp[a]->val=b;
-        mp[b]->val=a;
+        inorder(root);
+        if(first && last) swap(first->val,last->val);
+        else swap(first->val,middle->val);
     }
 };
