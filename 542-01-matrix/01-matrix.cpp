1d27bd31d9e9a029d8d8168dc6f35ddf2344aecf
Tina Borundia <168854094+TBorundia@users.noreply.github.com>
2025-08-29 18:56:57 +0530

    Time: 34 ms (16.30%) | Memory: 38.8 MB (44.09%) - LeetSync

---
+++ 542-01-matrix/01-matrix.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m=mat.size();
+        int n=mat[0].size();
+        
+        vector<vector<int>> vis(m,vector<int>(n,0));
+        vector<vector<int>> res(m,vector<int>(n,0));
+        queue<pair<pair<int,int>,int>> q;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==0){
+                    vis[i][j]=1;
+                    res[i][j]=0;
+                    q.push({{i,j},0});
+                }
+            }
+        }
+
+        int delr[]={-1,0,1,0};
+        int delc[]={0,1,0,-1};
+        while(!q.empty()){
+            int r=q.front().first.first;
+            int c=q.front().first.second;
+            int d=q.front().second;
+            q.pop();
+
+            for(int i=0;i<4;i++){
+                int nr=r+delr[i];
+                int nc=c+delc[i];
+
+                if(nr>=0 && nr<m && nc>=0 && nc<n && !vis[nr][nc] && mat[nr][nc]!=0){
+                    q.push({{nr,nc},d+1});
+                    vis[nr][nc]=1;
+                    res[nr][nc]=d+1;
+                }
+            }
+        }
+        return res;
+    }
+};
